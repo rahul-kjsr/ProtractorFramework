b3adbe43addfafe4a148f026c6eb4ea731fb2339
rahul-kjsr <rahul.kevincooper@gmail.com>
2020-01-04 00:48:57 +0530

    change calculator.js

--- tests/calculator.js
+++ tests/calculator.js
@@ -14,7 +14,7 @@ describe('demo calculator tests', function () {
 
         homepage.verifyResult('7');
 
-        browser.sleep(2000);
+        browser.sleep(3000);
     });
 
     it('subtraction test', function () {
